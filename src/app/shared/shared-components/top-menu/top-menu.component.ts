60d84a2134ceafe38f10b704487828ef5feb909a
unknown <uthayakumarthenujan@gmail.com>
2024-04-12 12:31:23 +0530

    Notification Front-end

--- src/app/shared/shared-components/top-menu/top-menu.component.ts
+++ src/app/shared/shared-components/top-menu/top-menu.component.ts
@@ -1,60 +1,88 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NotificationService } from '../../../main-dashboard/components/user-profile/user-notification/notification.service';
+import { interval,timer } from 'rxjs';
+import { startWith, switchMap } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-top-menu',
   templateUrl: './top-menu.component.html',
   styleUrls: ['./top-menu.component.scss']
 })
-export class TopMenuComponent {
+export class TopMenuComponent implements OnInit {
 
   notifications: any[] = [];
   readNotifications: any[] = [];
   notificationCount: number = 0;
-  viewNotifications: boolean = false; // Initialize viewNotifications to false
-  Read: boolean= false;
-  Unread : boolean=true;
+  ReadCount: number = 0;
+  UnreadCount: number = 1;
+  viewNotifications: boolean = false;
+  Read: boolean = false;
+  Unread: boolean = true;
 
   constructor(private notificationService: NotificationService) {}
 
   ngOnInit(): void {
     this.fetchNotifications();
+    timer(0, 1200).subscribe(() => {
+      if(this.Unread){
+        this.fetchNotifications();
+      }
+      
+    });
+    this.NotificationsCount();
   }
 
   fetchNotifications() {
     this.notificationService.getNotifications().subscribe(
       (notifications) => {
         this.notifications = notifications;
-        this.notificationCount = notifications.length;
         if (this.notificationCount === 0) {
-          this.notifications = [{"alert":"No Notifications","email":"Empty"}]; // Add this line
+          this.notifications = [{"alert":"No Notifications","email":"Empty"}];
         }
         this.Read = false;
-        this.Unread =true;
+        this.Unread = true;
       },
       (error) => {
         console.error('Error fetching notifications: ', error);
       }
-
-      
     );
   }
 
+  NotificationsCount() {
+    interval(1000) // Fetch notification count every 1 second
+      .pipe(
+        startWith(0), // Emit initial value to trigger the first fetch immediately
+        switchMap(() => this.notificationService.getNotifications())
+      )
+      .subscribe(
+        (notifications) => {
+          this.notificationCount = notifications.length;
+        },
+        (error) => {
+          console.error('Error fetching notification count: ', error);
+        }
+      );
+  }
+  
+
   showNotifications() {
-    this.viewNotifications = !this.viewNotifications; // Toggle viewNotifications
+    this.viewNotifications = !this.viewNotifications;
+    this.Read = false;
+    this.Unread = true;
   }
 
   markAsRead(notificationIndex: number) {
     const readNotification = this.notifications.splice(notificationIndex, 1)[0];
     this.readNotifications.push(readNotification);
-    this.notificationCount--;
-    console.log(readNotification.id);
+    if (this.notificationCount === 0) {
+      this.notifications = [{"alert":"No Notifications","email":"Empty"}];
+    }
     // Call the updateNotifications method with readNotifications
     this.notificationService.updateUnreadNotifications({"id": readNotification.id}).subscribe(
       (response) => {
+        this.UnreadCount = 1;
         console.log('Notifications updated successfully:', response);
-        // Clear readNotifications array after successful update if needed
-        // this.readNotifications = [];
       },
       (error) => {
         console.error('Error updating notifications:', error);
@@ -64,14 +92,14 @@ export class TopMenuComponent {
 
   markAsUnread(notificationIndex: number) {
     const ureadNotification = this.notifications.splice(notificationIndex, 1)[0];
-    this.notificationCount++;
-    console.log(ureadNotification.id);
+    this.UnreadCount = this.notifications.length;
+    if (this.notifications.length == 0) {
+      this.notifications = [{"alert":"No Notifications","email":"Empty"}];
+    }
     // Call the updateNotifications method with readNotifications
     this.notificationService.updateReadNotifications({"id": ureadNotification.id}).subscribe(
       (response) => {
         console.log('Notifications updated successfully:', response);
-        // Clear readNotifications array after successful update if needed
-        // this.readNotifications = [];
       },
       (error) => {
         console.error('Error updating notifications:', error);
@@ -83,14 +111,16 @@ export class TopMenuComponent {
     this.notificationService.getReadNotifications().subscribe(
       (notifications) => {
         this.notifications = notifications;
+        this.ReadCount = notifications.length;
+        if (this.ReadCount === 0) {
+          this.notifications = [{"alert":"No Notifications","email":"Empty"}];
+        }
         this.Read = true;
-        this.Unread =false;
+        this.Unread = false;
       },
       (error) => {
         console.error('Error fetching notifications: ', error);
       }
     );
   }
-
-
 }
